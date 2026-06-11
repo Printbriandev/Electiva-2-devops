17c66ac6e2de5ed3cdd8227853ede114bae1c649
Brian Amaury Guzman M. <guzmanmanonbrian@gmail.com>
2026-06-11 01:29:28 -0400

    primer commit hola mundo + Github Actions

---
+++ hola-mundo.js
@@ -0,0 +1,6 @@
+// Programa: Hola Mundo
+// Lenguaje: JavaScript (Node.js)
+// Práctica: Integración Continua con GitHub Actions
+
+console.log("¡Hola Mundo desde JavaScript!");
+console.log("Este programa fue ejecutado automáticamente por GitHub Actions.");
