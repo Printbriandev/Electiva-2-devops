043c21b5c67931c497fbdf2088e8ad2822aa042b
Brian Amaury Guzman M. <guzmanmanonbrian@gmail.com>
2026-06-11 02:19:56 -0400

    Eliminados

--- hola-mundo.js
+++
@@ -1,7 +0,0 @@
-// Programa: Hola Mundo
-// Lenguaje: JavaScript (Node.js)
-// Práctica: Integración Continua con GitHub Actions
-
-console.log("¡Hola Mundo desde JavaScript!");
-console.log("Este programa fue ejecutado automáticamente por GitHub Actions.");
-
